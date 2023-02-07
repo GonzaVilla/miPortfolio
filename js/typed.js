5cbb6c2416eddf9940c0796cd882c315646fdcc4
Gonzalo Villarroel <villarroelgonzalo1@gmail.com>
2023-02-07 08:17:48 -0300

    actualizacion del portafolio

---
+++ js/typed.js
@@ -0,0 +1,5 @@
+const typed = new Typed('.typed',{
+    strings: ["Cómo estás?, me llamo Gonzalo soy de Buenos aires, Argentina y estoy estudiando para ser programador, en esta web encontrarás apps desarrolladas por mí, mis objetivos como desarrollador mis pasiones, pasatiempos y muchas más!"],
+    typeSpeed: 80,
+    startDelay: 500,
+});
